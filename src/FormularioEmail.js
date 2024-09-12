c460b0d0508560cc7d81bf4f7ca007d3784476ce
Daniele Tavares Lobo <daniele.sistemas@lar.ind.br>
2024-09-12 12:16:28 -0300

    Formulário Email React

---
+++ src/FormularioEmail.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './FormularioEmail.css';
+
+const FormularioEmail = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Endereço de email inválido')
+        .required('O campo email é obrigatório'),
+    }),
+    onSubmit: (values) => {
+      alert(`Email enviado: ${values.email}`);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div style={{ color: 'red' }}>{formik.errors.email}</div>
+        ) : null}
+      </div>
+      <button type="submit">Enviar</button>
+    </form>
+  );
+};
+
+export default FormularioEmail;
+
